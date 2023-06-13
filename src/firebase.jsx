a3350d1466835176664c534804d5f2f3061e2cfe
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-13 12:09:47 +0900

    :heavy_plus_sign: Firebase 셋업

---
+++ src/firebase.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMcpzvoC_2bR5HAqwGxCGycunAty5z5Z4",
+  authDomain: "twitter-app-77b6e.firebaseapp.com",
+  projectId: "twitter-app-77b6e",
+  storageBucket: "twitter-app-77b6e.appspot.com",
+  messagingSenderId: "989542265115",
+  appId: "1:989542265115:web:897d0be3e00b97d962a3be"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
