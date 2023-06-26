caa6e185f00eba3c07dd38c3447fd46c96a05bc7
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-27 00:09:55 +0900

    :sparkles: Tweet 올릴 때 이미지 파일 업로드하는 기능 추가
    아직 업로드 한 이미지가 tweet에 보이지는 않고, Firebase Storage에 정상적으로 등록되는 것까지 확인

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,6 @@
 import uuid from "react-uuid";
 import { useEffect, useState } from "react";
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import {
   addDoc,
   collection,
@@ -8,12 +8,13 @@ import {
   onSnapshot,
   orderBy,
 } from "firebase/firestore";
-import { ref, uploadString } from "@firebase/storage";
+import { ref, uploadString } from "firebase/storage";
 import Tweet from "components/Tweet";
 
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
+  const [attachment, setAttachment] = useState(null);
   
   useEffect(() => {
     // query 불러오고 정렬하기
@@ -37,7 +38,8 @@ const Home = ({ userObj }) => {
     const fileRef = ref(storageService, `${userObj.uid}/${uuid()}`);
     const response = await uploadString(fileRef, attachment, "data_url");
     console.log(response);
-    
+
+    // 기존 코드
     // try {
     //   // "tweets" : collection 이름
     //   const docRef = await addDoc(collection(dbService, "tweets"), {
@@ -54,13 +56,47 @@ const Home = ({ userObj }) => {
 
   const onChange = (e) => {setTweet(e.target.value)}
 
+  const onFileChange = (e) => {
+    // Uncaught TypeError: Failed to execute 'readAsDataURL' on 'FileReader': parameter 1 is not of type 'Blob'. 오류 발생
+    // const {
+    //   target: { files },
+    // } = e;
+    const files = e.target?.files;
+
+    // fileReader API
+    // 1. input에 있는 모든 파일 중에 첫번째 파일만 받도록 한다.
+    const theFile = files[0];
+    // 2. 그 파일로 reader를 만든 후
+    const imgFileReader = new FileReader();
+    // 3. reader에 event listner를 추가한다.
+    imgFileReader.onloadend = (finishedEvent) => {
+      const { currentTarget: { result }
+        , } = finishedEvent;
+      // 브라우저가 읽을 수 있는 파일명을 state에 저장
+      setAttachment(result);
+    }
+    // 4. readAsDataURL API로 사진을 얻는다.
+    imgFileReader.readAsDataURL(theFile);
+  }
+  const onClearAttatchment = () => setAttachment(null);
+
   return (
     <>
       <h1>Home</h1>
       <div>
         <form onSubmit={onSubmitTweet}>
-          <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+          <input
+            value={tweet}
+            onChange={onChange}
+            type="text"
+            placeholder="What's on your mind?"
+            maxLength={120} />
+          <input type="file" accept="image/*" onChange={onFileChange} />
           <input type="submit" value="Tweet" />
+          {attachment && <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttatchment}>✖️</button>
+          </div>}
         </form>
         <div>
           {tweets.map((tweet) => (
