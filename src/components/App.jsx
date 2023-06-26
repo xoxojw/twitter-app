1f34d4a911d9328c068dbc4cf547548ea446ed72
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-26 11:01:39 +0900

    :sparkles: Tweet 게시글 작성, 추가한 게시글 화면에 보이기

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,24 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const App = () => {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // tweet을 누가 썼는지 확인하기 위해 현재 로그인 되어있는 유저 state 관리
+  const [userObj, setUserObj] = useState(null);
 
   useEffect(() => {
     const auth = getAuth();
+    // 로그인이 되면, onAuthS(tateChanged 함수가 호출됨
     onAuthStateChanged(auth, (user) => { 
       if (user) {
-        setIsLoggedIn(true);
+        // 로그인이 되면 userObj의 state가 초기값 null에서 userObj로 변경
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
+
+        // AppRouter에 위의 두 정보(로그인 여부, 유저명)를 props로 전달해줘야함
       } else {
-        setIsLoggedIn(false);
+        setUserObj(null);
       }
       setInit(true);
      });
@@ -20,7 +29,7 @@ const App = () => {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initializing..."}
       <footer>&copy; Twitter-clone-app {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,75 @@
-import React from "react"
+import { useEffect, useState } from "react";
+import { dbService } from "fbase";
+import {
+  addDoc,
+  collection,
+  query,
+  onSnapshot,
+  orderBy,
+  } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+  const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  
+  useEffect(() => {
+    // query 불러오고 정렬하기
+    const q = query(
+      collection(dbService, "tweets"),
+      orderBy("createdAt", "desc"),
+    );
+
+    // snapshot
+    onSnapshot(q, (snapshot) => {
+    const tweetArr = snapshot.docs.map((document) => ({
+      creatorId: 12121,
+      id: document.id,
+      ...document.data(),
+      }));
+      setTweets(tweetArr);
+    });
+    }, []);
+
+  const onSubmitTweet = async (e) => {
+    e.preventDefault();
+    try {
+      // "tweets" : collection 이름
+      const docRef = await addDoc(collection(dbService, "tweets"), {
+        text: tweet,
+        createdAt: Date.now(),
+        creatorId: userObj.uid,
+      });
+      console.log("Tweet written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding tweet: ", error);
+    }
+    setTweet("");
+  }
+
+  // console.log(tweets);
+
+  const onChange = (e) => {setTweet(e.target.value)}
+
+  return (
+    <>
+      <h1>Home</h1>
+      <div>
+        <form onSubmit={onSubmitTweet}>
+          <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+          <input type="submit" value="Tweet" />
+        </form>
+        <div>
+          {tweets.map((tweet) => {
+            return (
+              <div key={tweet.id}>
+                <h4>{tweet.text}</h4>
+                <p>{new Date(tweet.createdAt).toLocaleString()}</p>
+              </div>)
+          })}
+        </div>
+      </div>
+    </>
+  )
+};
 
 export default Home;
